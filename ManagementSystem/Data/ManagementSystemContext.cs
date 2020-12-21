d9887a1925606a7fb582c4e9fd8029dd2b9a1fbe
Joakim Malmström <35744406+Joakimmalmstrom@users.noreply.github.com>
2020-12-21 09:21:10 +0100

    Add project files.

---
+++ ManagementSystem/Data/ManagementSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ManagementSystem.Models.Entities;
+
+namespace ManagementSystem.Data
+{
+    public class ManagementSystemContext : DbContext
+    {
+        public ManagementSystemContext (DbContextOptions<ManagementSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ManagementSystem.Models.Entities.Device> Device { get; set; }
+    }
+}
---
+++ ManagementSystem/Migrations/20201221081850_Init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManagementSystem.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Device",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Device");
+        }
+    }
+}
